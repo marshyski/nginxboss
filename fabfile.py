aabc8e2f32bbb6ae1e4b1efce2848831c365b89d
Tim Ski <marshyski@gmail.com>
2014-12-18 11:08:38 -0500

    Added Fabric file

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from fabric.api import sudo, run, env, cd
+
+env.warn_only = True
+
+def setup_nginx():
+    git = run('rpm -qa | grep ^git')
+    if git == '':
+	sudo('yum install -y --nogpgcheck git')
+    with cd ('/tmp'):
+	run('git clone https://github.com/marshyski/nginxboss.git')
+	sudo('puppet apply --modulepath=/tmp -e "include nginxboss"')
